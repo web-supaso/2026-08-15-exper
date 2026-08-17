8f0c9209b47eed1e80279c542a90d8db7fc4a085
Supaso Credencial <credencial.supaso@gmail.com>
2026-08-16 23:23:36 -0300

    chore: bump footer version to v.04

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -154,7 +154,7 @@ export const Footer: React.FC<FooterProps> = ({
                 AMABLE
               </span>
             </a>
-            <span className="text-[10px] text-slate-500 font-mono tracking-tight">v.03</span>
+            <span className="text-[10px] text-slate-500 font-mono tracking-tight">v.04</span>
           </div>
         </div>
       </div>
